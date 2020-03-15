0094899128ba7b7a98e145f93511d53b4b379d56
Amber Williams <awilliams1234@live.com>
2020-03-15 14:14:31 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,14 +7,19 @@ class CashRegister
     @discount = discount
     @items = []
   end 
-    
-    def add_item(title, amount, quantity=1)
-      self.total += amount * quantity
-      quantity.times do
-        items << title
-      end
-      self.last_transaction = amount * quantity
-    end
+  
+  def add_item(title, amount, quantity=1)
+    self.total += amount * quantity
+    quantity.times do 
+      items << title 
+    end 
+    self.last_transaction = amount * quantity
+  end 
+  
+  def apply_discount
+    if discount != 0
+      self.total = 
+  
     
     def apply_discount
       if discount != 0
